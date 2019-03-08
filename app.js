8b08ec16140fb3afe5dad287fbf796098727e76a
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-03-07 22:49:32 -0500

    routes add

--- app.js
+++ app.js
@@ -1,27 +1,56 @@
-//var express = require('express');
 var serve = require('koa-static');
+var send = require('koa-send');
+const bodyParser = require('koa-bodyparser');
+
 var koa = require('koa');
-var app = new koa();//express();
+var app = new koa();
+
+app.use(bodyParser());
 var path = require('path');
 var server = require('http').createServer(app.callback());
 var io = require('socket.io')(server);
 
+const mongoose = require('mongoose');
+
+const historyRouter = require('./routes/history');
+const eventlogRouter = require('./routes/eventlog');
+
+// get mongodb connection
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://raj:raj667@ds117829.mlab.com:17829/chat-app', { useNewUrlParser: true });
+const db = mongoose.connection;
+app.db = db;
+db.on('error', console.error.bind(console, 'connection error:'));
+//console.log('mongodb connected');
+
 const PORT = process.env.PORT || 5002;
 
-var userList = [] ;
+var userList = [];
 
-
-// Routing
-//app.use(app.static(path.join(__dirname, 'public')));
 app.use(serve(path.join(__dirname, 'public')));
 
+app.use(historyRouter.routes()).use(historyRouter.allowedMethods());
+app.use(eventlogRouter.routes()).use(eventlogRouter.allowedMethods());
+
+app.use(function* index() {
+    yield send(this, __dirname + '/public/index.html');
+});
 
 //Socket listen by server
 
-var numuser = 0 ;
-//var messageList = [];
+var numuser = 0;
 
-const roomList = [{id: 1, name: 'room1'}, {id: 2, name: 'room2'}]
+const roomList = [{ id: 1, name: 'Alberta' },
+    { id: 2, name: 'British Columbia' },
+    { id: 3, name: 'Manitoba' },
+    { id: 4, name: 'New Brunswick' },
+    { id: 5, name: 'Nova Scotia' },
+    { id: 6, name: 'Ontario' },
+    { id: 7, name: 'Prince Edward Island' },
+    { id: 8, name: 'Quebec' },
+    { id: 9, name: 'Saskatchewan' },
+    { id: 10, name: 'Yukon' }
+]
 const chatMessage = [];
 
 const addChatMessage = function(user, message, room) {
@@ -36,66 +65,90 @@ const addChatMessage = function(user, message, room) {
 io.on('connection', function(socket) {
     socket.on('joinroom', (roomname) => {
         socket.join(roomname);
-        socket.roomList.push({'roomname': roomname, 'username': socket.userName});
+        socket.roomList.push({ 'roomname': roomname, 'username': socket.userName });
         socket.roomName = roomname;
-        io.sockets.to(roomname).emit('joinroom', socket.userName, socket.roomName)
-    });
-    socket.on('register user', (v) => { //, (v, callback)
-       const email = v.email;//v.split('&')[0].split('=')[1];
-       socket.userName = email;
-       checkEmail(email, (r) => {
-           if (!r) {
-               const emailObject = {'email': email};
-               userList.push(emailObject);
-               socket.userName = email;
-               socket.roomList = roomList;
-               socket.chatMessage = chatMessage;
-               
-               addChatMessage(email, 'Joined', null);
+        io.sockets.to(roomname).emit('joinroom', socket.userName, socket.roomName);
 
-               io.sockets.emit('register user', socket.userName, socket.roomList, socket.chatMessage);
-           }
-           else {
-               console.log('already done' +  email);
-           }
-       })
+        eventLog('joinroom', socket.userName);
+    });
+
+    socket.on('register user', (v) => {
+
+        const email = v.email;
+        const nickname = v.nickName;
+        socket.userName = email;
+        socket.nickName = nickname;
+
+        checkEmail(email, (r) => {
+            if (!r) {
+                const emailObject = { 'email': email };
+                userList.push(emailObject);
+                socket.userName = email;
+                socket.nickName = nickname;
+                socket.roomList = roomList;
+                socket.chatMessage = chatMessage;
+
+
+                addChatMessage(email, 'Joined', null);
+
+                io.sockets.emit('register user', socket.userName, socket.nickName, socket.roomList, socket.chatMessage);
+            } else {
+                console.log('already done' + email);
+            }
+        })
+        eventLog('Connection', socket.userName);
     });
 
     //Send message 
     socket.on('sendmessage', (v) => {
-       const room = socket.roomList.find(x => x.username === socket.userName);
-       if (room === undefined) {
-        io.sockets.emit('sendmessage', v);
-       }
-       else {
+        const room = socket.roomList.find(x => x.username === socket.userName);
+        let chatHistorySchema = {
+            username: v.user,
+            usernick: v.nick,
+            message: v.message,
+            roomName: null
+        };
+        if (room === undefined) {
+            db.collection('chathistory').insertOne(chatHistorySchema);
+            io.sockets.emit('sendmessage', v);
+        } else {
+            chatHistorySchema.roomName = room.roomname;
+            db.collection('chathistory').insertOne(chatHistorySchema);
             io.sockets.to(room.roomname).emit('sendmessagetoroom', v);
-       }
+        }
     });
 
     // disconnect 
     socket.on('disconnect', () => {
-        
+
         io.sockets.emit('disconnect', socket.userName);
+        eventLog('Disconnect', socket.userName);
     });
 });
 
 
 /* General function */
-const checkEmail = function(email, callback)
-{
-    if (userList.length > 0)
-    {
+const checkEmail = function(email, callback) {
+    if (userList.length > 0) {
         const user = userList.find(x => x.email === email);
-        if ( user === undefined) {
+        if (user === undefined) {
             callback(false);
-        }
-        else {
+        } else {
             callback(true);
         }
+    } else callback(false);
+}
+
+const eventLog = function(event, username) {
+    const _eventlog = {
+        'event': event,
+        'date': new Date(),
+        'time': now.getTime(),
+        'user': username
     }
-    else callback(false);
+    db.collection('eventlogs').insert(_eventlog);
 }
 
 server.listen(PORT, () => {
-  console.log('Server is running on PORT %d', PORT);
+    console.log('Server is running on PORT %d', PORT);
 });
--- public/main.js
+++ public/main.js
@@ -1,73 +1,72 @@
-   var app  = angular.module('myApp', []);
+   var app = angular.module('myApp', []);
    app.controller('myCtrl', function($scope) {
 
-    $scope.logInSection = true;
-    $scope.chatSection = false;
-    $scope.chatSectionLeftRoom = false;
-    $scope.chatSectionLeft = false;
-    $scope.login = {
-      email: 'jyotiad@a.com',
-      nickName: 'jyoti'
-    }
-    $scope.roomList = [];
-    $scope.chatMessage = [];
-    $scope.privatechatMessage = [];
-    $scope.message = '';
-    
-    /* socket */
-    var socket = io();
+       $scope.logInSection = true;
+       $scope.chatSection = false;
+       $scope.chatSectionLeftRoom = false;
+       $scope.chatSectionLeft = false;
+       $scope.login = {
+           email: '',
+           nickName: ''
+       }
+       $scope.roomList = [];
+       $scope.chatMessage = [];
+       $scope.privatechatMessage = [];
+       $scope.message = '';
 
-    /* Event */
-    $scope.onLoginClick = () => {
-      socket.emit('register user', $scope.login);
-      $scope.logInSection = false;
-      $scope.chatSection = true;
-      $scope.chatSectionLeft = true;
-    }
+       /* socket */
+       var socket = io();
 
-    $scope.onClickSend = () => {
-      if ($scope.message != '')
-      {
-        socket.emit('sendmessage', {'user': $scope.login.email, 'message': $scope.message});
-        $scope.message = '';
-      }
-    }
+       /* Event */
+       $scope.onLoginClick = () => {
+           socket.emit('register user', $scope.login);
+           $scope.logInSection = false;
+           $scope.chatSection = true;
+           $scope.chatSectionLeft = true;
+       }
 
-    $scope.onClickRoom = (room) => {
-      const roomName = room.name;
-      if (confirm('Do you want to Join ' + roomName)) {
-         socket.emit('joinroom', roomName);
-      }
-    }
+       $scope.onClickSend = () => {
+           if ($scope.message != '') {
+               socket.emit('sendmessage', { 'user': $scope.login.email, 'nick': $scope.login.nickName, 'message': $scope.message, 'room': $scope.roomName });
+               $scope.message = '';
+           }
+       }
 
-    socket.on('register user', (username, roomlist, chatmessage) => {
-        $scope.$apply(function () {
-          $scope.roomList = roomlist;
-          $scope.chatMessage.push({'user' : username, 'message': 'Joined'});
-        });
-    });
+       $scope.onClickRoom = (room) => {
+           const roomName = room.name;
+           if (confirm('Do you want to Join ' + roomName)) {
+               socket.emit('joinroom', roomName);
+           }
+       }
 
-    socket.on('sendmessage', (v) => {
-      $scope.$apply(function () {
-        $scope.chatMessage.push(v);
-      });
-    });
+       socket.on('register user', (username, usernick, roomlist, chatmessage) => {
+           $scope.$apply(function() {
+               $scope.roomList = roomlist;
+               $scope.chatMessage.push({ 'user': username, 'nick': usernick, 'message': 'Joined' });
+           });
+       });
 
-    socket.on('joinroom', (username, roomname) => {
-      $scope.$apply(function () {
-        $scope.chatSectionLeft = false;
-        $scope.chatSectionLeftRoom = true;
-        $scope.privatechatMessage.push({'user': username, 'message': ' Joined ' + roomname});
-      });
-    });
+       socket.on('sendmessage', (v) => {
+           $scope.$apply(function() {
+               $scope.chatMessage.push(v);
+           });
+       });
 
-    socket.on('sendmessagetoroom', (v) => {
-      $scope.$apply(function () {
-        $scope.privatechatMessage.push(v);
-      });
-    })
+       socket.on('joinroom', (username, roomname) => {
+           $scope.$apply(function() {
+               $scope.chatSectionLeft = false;
+               $scope.chatSectionLeftRoom = true;
+               $scope.privatechatMessage.push({ 'user': username, 'message': ' Joined ' + roomname });
+           });
+       });
 
-    socket.on('disconnect', (v) => {
-      $scope.chatMessage.push({'user' : v, 'message': ' Leaves'});
-    });
-});
+       socket.on('sendmessagetoroom', (v) => {
+           $scope.$apply(function() {
+               $scope.privatechatMessage.push(v);
+           });
+       })
+
+       socket.on('disconnect', (v) => {
+           $scope.chatMessage.push({ 'user': v, 'message': ' Leaves' });
+       });
+   });
